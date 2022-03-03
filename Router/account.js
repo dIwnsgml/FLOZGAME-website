68b347689a3f11bfa784204fc261bb2908533939
BillGray <junjason1126@gmail.com>
2022-03-03 05:53:49 -0800

    hash_salt

--- Router/account.js
+++ Router/account.js
@@ -1,129 +1,172 @@
 var express = require('express');
 var router = express.Router();
-var connection  = require('../model/db');
+var connection = require('../model/db');
+const crypto = require("crypto");
+const util = require("util");
+const { response } = require('express');
+
+/* const randomBytesPromise = util.promisify(crypto.randomBytes);
+const pbkdf2Promise = util.promisify(crypto.pbkdf2);
+
+const createSalt = function(){
+  const buf = randomBytesPromise(64);
+
+  return buf.toString("base64");
+}; */
+
+/* const createHashedPassword = function(password){
+  const salt = createSalt();
+  const key = pbkdf2Promise(password, salt, 984316, 64, "sha512");
+  const hashedPassword = key.toString("base64");
+
+  return hashedPassword;
+}; */
+
+//console.log(createSalt());
 
 
-router.get('/', function(req, res, next){
+function hashTest(password) {
+  var salt = crypto.randomBytes(32).toString('hex')
+  return [salt, crypto.pbkdf2Sync(password, salt, 99097, 32, 'sha512').toString('hex')]
+}
+
+// 암호화
+
+//console.log(createHashedPassword('as'));
+
+router.get('/', function (req, res, next) {
   //res.write("<script>window.location=\"../login\"</script>");
   res.render("login");
 })
 //display login page
-router.get('/login', function(req, res, next){    
-// render to views/user/add.ejs
-res.render('login', {
-title: 'Login',
-email: '',
-password: ''    
-})
+router.get('/login', function (req, res, next) {
+  // render to views/user/add.ejs
+  res.render('login', {
+    title: 'Login',
+    email: '',
+    password: ''
+  })
 })
 //authenticate user
-router.post('/authentication', function(req, res, next) {
-var email = req.body.email;
-var password = req.body.password;
-connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(err, rows, fields) {
-//if(err) throw err
-// if user not found
-if (rows.length <= 0) {
-req.flash('error', 'Please correct enter email and Password!')
-res.redirect('/account/login')
-}
-else { // if user found
-// render to views/user/edit.ejs template file
-req.session.loggedin = true;
-var name = req.session.name;
-res.redirect('/');
-}            
-})
+router.post('/authentication', function (req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var b = hashTest(password);
+  connection.query('SELECT * FROM users WHERE email = ?', [email], function (err, rows, fields, salt, results) {
+    //if(err) throw err
+    // if user not found
+    console.log(rows[0].salt, crypto.pbkdf2Sync(password, rows[0].salt, 99097, 32, 'sha512').toString('hex'), rows[0].password);
+
+    if (crypto.pbkdf2Sync(password, rows[0].salt, 99097, 32, 'sha512').toString('hex') == rows[0].password) {
+
+      // if user found
+      // render to views/user/edit.ejs template file
+      req.session.loggedin = true;
+      var name = req.session.name;
+      res.redirect('/');
+    }
+    else {
+      req.flash('error', 'Please correct enter email and Password!')
+      res.redirect('/account/login')
+    }
+  })
 })
 //display login page
-router.get('/register', function(req, res, next){    
-// render to views/user/add.ejs
-res.render('register', {
-title: 'Registration Page',
-name: '',
-email: '',
-password: ''    
-})
+router.get('/register', function (req, res, next) {
+  // render to views/user/add.ejs
+  res.render('register', {
+    title: 'Registration Page',
+    name: '',
+    email: '',
+    password: ''
+  })
 })
 // user registration
-router.post('/post-register', function(req, res, next){ 
-req.assert('name', 'Name is required').notEmpty()           //Validate name
-req.assert('password', 'Password is required').notEmpty()   //Validate password
-req.assert('email', 'A valid email is required').isEmail()  //Validate email
-var email = req.body.email;
-var errors = req.validationErrors();
+router.post('/post-register', function (req, res, next) {
+  req.assert('name', 'Name is required').notEmpty()           //Validate name
+  req.assert('password', 'Password is required').notEmpty()   //Validate password
+  req.assert('email', 'A valid email is required').isEmail()  //Validate email
+  var email = req.body.email;
+  var password = req.body.password;
+  var errors = req.validationErrors();
+  var a = hashTest(password);
+  console.log(a, a[0], a[1]);
+  if (!errors) {   //No errors were found.  Passed Validation!
+    connection.query("SELECT * FROM users WHERE email = ?", email, function (err, result, field) {
+      if (result.length == 0) {
+        console.log("new");
+        var user = {
+          name: req.sanitize('name').escape().trim(),
+          email: req.sanitize('email').escape().trim(),
+          password: a[1],
+          salt: a[0],
 
-
-if( !errors ) {   //No errors were found.  Passed Validation!
-  connection.query("SELECT * FROM users WHERE email = ?", email, function(err, result, field){
-    if(result.length == 0){
-      console.log("new");
-      var user = {
-        name: req.sanitize('name').escape().trim(),
-        email: req.sanitize('email').escape().trim(),
-        password: req.sanitize('password').escape().trim()
+          //req.sanitize('password').escape().trim(),
         }
+        console.log(hashTest(password));
         console.log("passwd");
-        connection.query('INSERT INTO users SET ?', user, function(err, result) {
-        //if(err) throw err
-        if (err) {
-        req.flash('error', err)
-        // render to views/user/add.ejs
-        res.render('register', {
-        title: 'Registration Page',
-        name: '',
-        password: '',
-        email: ''                   
+        connection.query('INSERT INTO users SET ?', user, function (err, result) {
+          //if(err) throw err
+          if (err) {
+            req.flash('error', err)
+            // render to views/user/add.ejs
+            res.render('register', {
+              title: 'Registration Page',
+              name: '',
+              password: '',
+              email: ''
+            })
+          } else {
+            req.flash('success', 'You have successfully signup!');
+            res.write("<script>alert('success')</script>");
+            res.write("<script>window.location=\"/account/login\"</script>");
+          }
         })
-        } else {                
-        req.flash('success', 'You have successfully signup!');
-        res.write("<script>alert('success')</script>");
-        res.write("<script>window.location=\"/account/login\"</script>");
-        }
-        })
-  } else {
-    console.log("not new");
-    res.write("<script>alert('already exist')</script>");
-res.write("<script>window.location=\"/account/register\"</script>");
+      } else {
+        console.log("not new");
+        res.write("<script>alert('already exist')</script>");
+        res.write("<script>window.location=\"/account/register\"</script>");
+        /* res.redirect("/account/register");
+        req.flash('error', 'already exist'); */
+      }
+    });
+    console.log(email);
   }
-  });
-  console.log(email);
-}
-else {   //Display errors to user
-var error_msg = ''
-errors.forEach(function(error) {
-error_msg += error.msg + '<br>'
-})                
-req.flash('error', error_msg)        
-/**
-* Using req.body.name 
-* because req.param('name') is deprecated
-*/
-res.render('register', { 
-title: 'Registration Page',
-name: req.body.name,
-email: req.body.email,
-password: ''
-})
-}
+  else {   //Display errors to user
+    var error_msg = ''
+    errors.forEach(function (error) {
+      error_msg += error.msg + '<br>'
+    })
+    req.flash('error', error_msg)
+    /**
+    * Using req.body.name 
+    * because req.param('name') is deprecated
+    */
+    res.render('register', {
+      title: 'Registration Page',
+      name: req.body.name,
+      email: req.body.email,
+      password: ''
+    })
+  }
 })
 //display home page
-router.get('/home', function(req, res, next) {
-if (req.session.loggedin) {
-res.render('home', {
-title:"Dashboard",
-name: req.session.name,     
-});
-} else {
-req.flash('success', 'Please login first!');
-res.redirect('/account/login');
-}
+router.get('/home', function (req, res, next) {
+  if (req.session.loggedin) {
+    res.render('home', {
+      title: "Dashboard",
+      name: req.session.name,
+    });
+  } else {
+    req.flash('success', 'Please login first!');
+    res.redirect('/account/login');
+  }
 });
 // Logout user
 router.get('/logout', function (req, res) {
-req.session.destroy();
-//req.flash('success', 'Login Again Here');
-res.redirect('/');
+  req.session.destroy();
+  //req.flash('success', 'Login Again Here');
+  res.redirect('/');
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app= express();
+const app = express();
 const port = 3000;
 const ejs = require("ejs");
 const createError = require("http-errors");
@@ -14,7 +14,19 @@ const mysql = require("mysql");
 const connection = require("./model/db");
 const helmet = require("helmet");
 
-app.use(helmet());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+//app.use(helmet.noSniff());
+//app.use(helmet.contentSecurityPolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+
+
 const mainRouter = require("./Router/main");
 const accountRouter = require("./Router/account");
 const gameRouter = require("./Router/game");
@@ -29,14 +41,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
- 
-app.use(session({ 
-    secret: '123456cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 20 }
+
+app.use(session({
+  secret: '123456cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 20 }
 }))
- 
+
 app.use(flash());
 app.use(expressValidator());
 
@@ -45,16 +57,16 @@ app.use('/account', accountRouter);
 app.use('/games', gameRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
- 
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
+
   // render the error page
   res.status(err.status || 500);
   res.render(err);
